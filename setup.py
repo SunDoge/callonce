13bea34aa041e41a3c564e1aa0e5dda262dea4e9
SunDoge <384813529@qq.com>
2020-10-14 17:39:25 +0800

    refactor(setup.py): prepare to publish

--- setup.py
+++ setup.py
@@ -4,9 +4,23 @@ from setuptools import find_packages, setup
 
 from callonce._version import __version__
 
+
+def read_long_description():
+    with open('README.md', 'r') as f:
+        long_description = f.read()
+
+    return long_description
+
+
 setup(
     name='callonce',
     version=__version__,
+    author='SunDoge',
+    author_email='384813529@qq.com',
     description='make a function callable for once or N times',
-    packages=find_packages(exclude=['tests'])
+    long_description=read_long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SunDoge/callonce',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
 )
